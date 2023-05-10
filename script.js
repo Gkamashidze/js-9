8c20c429d4e561c914e3343ccfd43d5a8e6ef4ab
Gkamashidze <gqamashidze51@gmail.com>
2023-05-10 22:03:36 +0400

    განახლება 1.0

---
+++ script.js
@@ -0,0 +1,36 @@
+function damateba() {
+
+    num1 = document.getElementById("number-one").value;
+
+    num2 = document.getElementById("number-two").value;
+
+    document.getElementById("pasuxi").innerHTML = Number(num1) + Number(num2);
+
+}
+
+function gamokleba() {
+
+    num1 = document.getElementById("number-one").value;
+
+    num2 = document.getElementById("number-two").value;
+
+    document.getElementById("pasuxi").innerHTML = num1 - num2;
+}
+
+function gamravleba() {
+
+    num1 = document.getElementById("number-one").value;
+
+    num2 = document.getElementById("number-two").value;
+
+    document.getElementById("pasuxi").innerHTML = num1 * num2;
+}
+
+function gayofa() {
+
+    num1 = document.getElementById("number-one").value;
+
+    num2 = document.getElementById("number-two").value;
+
+    document.getElementById("pasuxi").innerHTML = num1 / num2;
+}
